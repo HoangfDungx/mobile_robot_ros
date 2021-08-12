ea5d4c7c73734196099820a940e691f1805f3c5f
HOANG DUNG <hoangdung@ubuntu.ubuntu-domain>
2021-08-12 17:12:38 +0700

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/hoangdung/ROS_project/mobile_robot_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
